66bccbe59e688cb6f659cc040aebe0933a15ccb7
Mitch Hiltz <mitch.hiltz@gmail.com>
2020-06-10 11:14:21 -0400

    Laptop class fleshed out

--- Technology/Technology/Laptop.cs
+++ Technology/Technology/Laptop.cs
@@ -6,6 +6,42 @@ namespace Technology
 {
     class Laptop : Computer
     {
+        public double TotalBatteryLifeInHours { get; set; }
+        public double RemainingBatteryLife { get; set; }
 
+        public Laptop(string manufacturer, string operatingSystem, int storageSpaceInGigabytes, int randomAccessMemoryInGigabytes, double totalBatteryLifeInHours) : base(manufacturer, operatingSystem, storageSpaceInGigabytes, randomAccessMemoryInGigabytes)
+        {
+            TotalBatteryLifeInHours = totalBatteryLifeInHours;
+            RemainingBatteryLife = TotalBatteryLifeInHours;
+        }
+
+        public Laptop(double totalBatteryLifeInHours)
+            : this("HP", "Windows 10", 500, 8, totalBatteryLifeInHours) {}
+
+        public Laptop()
+            : this("HP", "Windows 10", 500, 8, 10.0) {}
+
+        public void Use(double hours)
+        {
+            if (this.PoweredOnStatus == false)
+            {
+                this.PowerOn();
+            }
+            RemainingBatteryLife -= hours;
+            if (RemainingBatteryLife <= 0)
+            {
+                RemainingBatteryLife = 0;
+                this.PowerOff();
+            }
+        }
+
+        public void Charge(double hours)
+        {
+            RemainingBatteryLife += 1.5 * hours;
+            if (RemainingBatteryLife > TotalBatteryLifeInHours)
+            {
+                RemainingBatteryLife = TotalBatteryLifeInHours;
+            }
+        }
     }
 }
--- Technology/Technology/Program.cs
+++ Technology/Technology/Program.cs
@@ -6,12 +6,22 @@ namespace Technology
     {
         static void Main(string[] args)
         {
+            //TESTING THE COMPUTER CLASS
             //Computer newPC = new Computer();
             //Console.WriteLine($"Powered on: {newPC.PoweredOnStatus.ToString()}");
             //newPC.PowerOn();
             //Console.WriteLine($"Powered on: {newPC.PoweredOnStatus.ToString()}");
-
             //Console.WriteLine(newPC.Manufacturer);
+
+            //TESTING THE LAPTOP CLASS
+            //Laptop newLaptop = new Laptop(16.0);
+            //newLaptop.Use(18.0);
+            //Console.WriteLine($"Battery remaining: {newLaptop.RemainingBatteryLife} hour(s)");
+            //newLaptop.Charge(1);
+            //Console.WriteLine(newLaptop.TotalBatteryLifeInHours);
+            //Console.WriteLine(newLaptop.RemainingBatteryLife);
+
+            //TESTING THE SMARTPHONE CLASS
 
 
         }
